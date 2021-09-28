fd600c36e9b8f589eaa2dab611651b22947d5e8b
rlftr <rlftr@yandex.ru>
2021-09-28 13:19:22 +0300

    addstation
    добавлена станция

--- Kunitsyna_1/Kunitsyna_1/Kunitsyna_1.cpp
+++ Kunitsyna_1/Kunitsyna_1/Kunitsyna_1.cpp
@@ -36,9 +36,38 @@ pipe AddPipe() {
 
 }
 
+struct station {
+
+	string name;
+	int workshops;
+	int WorkshopsInOperation;
+	int efficiency;
+	int id;
+}
+
+
+; station AddStation() {
+
+	station s;
+	s.id = 1;
+	std::cout << "Enter the name: ";
+	std::cin >> s.name;
+	std::cout << "Enter the number of workshops: ";
+	std::cin >> s.workshops;
+	std::cout << "Enter the number of workshops in operation: ";
+	std::cin >> s.WorkshopsInOperation;
+	std::cout << "Enter the efficiency: ";
+	std::cin >> s.efficiency;
+	return s;
+
+}
+
+
+
 int main()
 {
 	pipe p = AddPipe();
+	station s = AddStation();
 }
 
 
