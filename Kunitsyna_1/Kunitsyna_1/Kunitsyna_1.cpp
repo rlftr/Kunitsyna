5a143193305cf6467eabc6d3e9861be9fa0c8233
rlftr <rlftr@yandex.ru>
2021-11-02 20:07:02 +0300

    correct

--- Kunitsyna_1/Kunitsyna_1/Kunitsyna_1.cpp
+++ Kunitsyna_1/Kunitsyna_1/Kunitsyna_1.cpp
@@ -3,6 +3,10 @@
 #include <fstream>
 #include <cstdlib>
 #include <istream>
+#include <string>
+#include <vector>
+#include <map>
+
 using namespace std;
 
 void menu()
@@ -18,6 +22,21 @@ void menu()
 		<< "0. Exit\n";
 }
 
+int rightValue() {
+	int enter;
+	while (true) {
+		std::cin >> enter;
+		if (cin.fail()) {
+			cin.clear();
+			cin.ignore(32767, '\n');
+			std::cout << "Error. Try again: ";
+		}
+		else {
+			return enter;
+		}
+	}
+}
+
 struct pipe {
 	int id;
 	int diametr;
@@ -25,120 +44,139 @@ struct pipe {
 	int repair;
 };
 
-pipe AddPipe() {
+
+ pipe PipeEdit(pipe& p) {
+	 if (p.id < 0) {
+		 cout << "Pipe wasn't added. Please add pipe.\n";
+	 }
+	 else {
+		 std::cout << "Is the pipe under repair? ('yes'- 1 or 'no'- 2): ";
+		 p.repair = rightValue();
+
+		 while (p.repair < 1 || p.repair > 2) {
+			 std::cout << "Please enter 1(yes) or 2(no): ";
+			 p.repair = rightValue();
+			 continue;
+		 }
+		 cout << "\nPipe was edited.";
+		 return p;
+		 
+	 }
+}
+
+
+pipe AddPipe(vector<pipe>& pipesVector) {
 
 	pipe p;
-	p.id = 0;
+	p.id = 1;
 	std::cout << "Enter the diametr: ";
-	std::cin >> p.diametr;
+	p.diametr = rightValue();
 
 	while (p.diametr <= 0) {
 		std::cout << "The value must be > 0. Try again: ";
-		std::cin >> p.diametr;
+		p.diametr = rightValue();
 		continue;
 	}
 
 	std::cout << "Enter the length: ";
-	std::cin >> p.length;
+	p.length = rightValue();
 
 	while (p.length <= 0) {
 		std::cout << "The value must be > 0. Try again: ";
-		std::cin >> p.length;
+		p.length = rightValue();
 		continue;
 	}
 
 	std::cout << "Is the pipe under repair? ('yes'- 1 or 'no'- 2):  ";
-	std::cin >> p.repair;
+	p.repair = rightValue();
 
 	while (p.repair < 1 || p.repair > 2) {
 		std::cout << "Please enter 1(yes) or 2(no): ";
-		std::cin >> p.repair;
+		p.repair = rightValue();
 		continue;
 	}
+	std::cout << "\nPipe was added.\n";
 	return p;
 }
 
 struct station {
+	int id;
 	string name;
 	int workshops;
 	int WorkshopsInOperation;
 	int efficiency;
-	int id;
+};
+
+station StationEdit(station& s) {
+		std::cout << "Enter the number of workshops in operation: ";
+		s.WorkshopsInOperation = rightValue();
+
+		while (s.WorkshopsInOperation < 0) {
+			std::cout << "The value must be >= 0. Try again: ";
+			s.WorkshopsInOperation = rightValue();
+			continue;
+		}
+		
+		while (s.WorkshopsInOperation > s.workshops) {
+			int e;
+			std::cout << "The value must be <= number of workshops. Maybe you didn't add station.\n";
+			std::cout << "Please enter 1(menu) or 2(try again): \n";
+			e = rightValue();
+			if (e == 1) {
+				break;
+			}
+			if (e == 2) {
+				std::cout << "Enter the number of workshops in operation: ";
+				s.WorkshopsInOperation = rightValue();
+				continue;
+			}
+		}
+		cout << "\nStation was edited.\n";
+		return s;
 }
 
 ; station AddStation() {
-
 	station s;
 	s.id = 1;
 	std::cout << "Enter the name: ";
-	std::cin >> s.name;
+	cin.ignore(32767, '\n');
+	getline(cin, s.name);
 	std::cout << "Enter the number of workshops: ";
-	std::cin >> s.workshops;
+	s.workshops = rightValue();
 
 	while (s.workshops < 0) {
 		std::cout << "The value must be > 0. Try again: ";
-		std::cin >> s.workshops;
+		s.workshops = rightValue();
 		continue;
 	}
 
 	std::cout << "Enter the number of workshops in operation: ";
-	std::cin >> s.WorkshopsInOperation;
+	s.WorkshopsInOperation = rightValue();
 
 	while (s.WorkshopsInOperation < 0) {
 		std::cout << "The value must be >= 0. Try again: ";
-		std::cin >> s.WorkshopsInOperation;
+		s.WorkshopsInOperation = rightValue();
 		continue;
 	}
 
 	while (s.WorkshopsInOperation > s.workshops) {
 		std::cout << "Number of working workshops can't be > number of workshops. Try again: ";
-		std::cin >> s.WorkshopsInOperation;
+		s.WorkshopsInOperation = rightValue();
 		continue;
 	}
 
 	std::cout << "Enter the efficiency (%): ";
-	std::cin >> s.efficiency;
+	s.efficiency = rightValue();
 
 	while (s.efficiency < 0 || s.efficiency > 100) {
 		std::cout << "The value must be >= 0 and <= 100. Try again: ";
-		std::cin >> s.efficiency;
+		s.efficiency = rightValue();
 		continue;
 	}
-
+	cout << "\nStation was added.\n";
 	return s;
-
 }
 
-pipe PipeEdit(pipe& p) {
-	std::cout << "Is the pipe under repair? ('yes'- 1 or 'no'- 2): ";
-	std::cin >> p.repair;
-
-	while (p.repair < 1 || p.repair > 2) {
-		std::cout << "Please enter 1(yes) or 2(no): ";
-		std::cin >> p.repair;
-		continue;
-	}
-	return p;
-}
-
-station StationEdit(station& s) {
-	std::cout << "Enter the number of workshops in operation: ";
-	std::cin >> s.WorkshopsInOperation;
-
-	while (s.WorkshopsInOperation < 0) {
-		std::cout << "The value must be >= 0. Try again: ";
-		std::cin >> s.WorkshopsInOperation;
-		continue;
-	}
-
-	while (s.WorkshopsInOperation > s.workshops) {
-		std::cout << "Number of working workshops can't be > number of workshops. Try again: ";
-		std::cin >> s.WorkshopsInOperation;
-		continue;
-	}
-	return s;
-
-}
 
 void Output(pipe p, station s) {
 	cout << "\nAll objects:\n";
@@ -147,7 +185,7 @@ void Output(pipe p, station s) {
 		cout << "\nId: " << p.id;
 		cout << "\nDiametr: " << p.diametr;
 		cout << "\nLength: " << p.length;
-		cout << "\nRepair ('yes' - 1, 'no' - 2)" << p.repair << "\n";
+		cout << "\nRepair ('yes' - 1, 'no' - 2): " << p.repair << "\n";
 	}
 
 	if (s.name != "") {
@@ -159,6 +197,7 @@ void Output(pipe p, station s) {
 		cout << "\nEfficiency: " << s.efficiency << "\n";
 	}
 }
+
 void Save(pipe p, station s) {
 	ofstream file;
 	file.open("database.txt");
@@ -168,7 +207,7 @@ void Save(pipe p, station s) {
 			file << "\nId: " << p.id;
 			file << "\nDiametr: " << p.diametr;
 			file << "\nLength: " << p.length;
-			file << "\nRepair ('yes' - 1, 'no' - 2)" << p.repair << "\n";
+			file << "\nRepair ('yes' - 1, 'no' - 2): " << p.repair << "\n";
 		}
 
 		if (s.name != "") {
@@ -184,29 +223,19 @@ void Save(pipe p, station s) {
 	}
 }
 
-void Load(pipe& p, station& s) {
-	ifstream file;
-	file.open("database.txt");
-	if (file.good()) {
-		while (!file.eof()) {
-			string type;
-			file >> type;
-			if (type == "Pipe:") {
-				file >> p.id;
-				file >> p.diametr;
-				file >> p.length;
-				file >> p.repair;
-			}
-			if (type == "Station:") {
-				file >> s.id;
-				file >> s.name;
-				file >> s.workshops;
-				file >> s.WorkshopsInOperation;
-				file >> s.efficiency;
-			}
-		}
+int Load(pipe& p, station& s) {
+	ifstream inf("database.txt");
+	if (!inf) {
+		cerr << "database.txt couldn't be opened" << endl;
+		exit(1);
 	}
-	cout << "Loaded\n";
+
+	while (inf) {
+		string strInput;
+		getline(inf, strInput);
+		cout << strInput << endl;
+	}
+	return 0;
 }
 
 int main()
@@ -222,7 +251,7 @@ int main()
 		cin >> a;
 		switch (a) {
 		case 1:
-			p = AddPipe();
+			// p = AddPipe();
 			continue;
 		case 2:
 			s = AddStation();
