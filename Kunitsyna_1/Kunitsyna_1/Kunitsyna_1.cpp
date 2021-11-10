e514bd8f6a4f1ba6123db5eae70687ecc3ef22a3
rlftr <rlftr@yandex.ru>
2021-11-10 22:37:36 +0300

    search pipe and station
    добавление возможности поиска трубы и станции

--- Kunitsyna_1/Kunitsyna_1/Kunitsyna_1.cpp
+++ Kunitsyna_1/Kunitsyna_1/Kunitsyna_1.cpp
@@ -5,7 +5,7 @@
 #include <istream>
 #include <string>
 #include <vector>
-#include <map>
+#include <algorithm>
 
 using namespace std;
 
@@ -19,6 +19,8 @@ void menu()
 		<< "5. Edit Station\n"
 		<< "6. Save\n"
 		<< "7. Load\n"
+		<< "8. Search Pipe\n"
+		<< "9. Search Station\n"
 		<< "0. Exit\n";
 }
 
@@ -42,6 +44,7 @@ struct pipe {
 	int diametr;
 	int length;
 	int repair;
+	string name;
 };
 
 bool checkId(int& Id, vector<pipe>& pipes) {
@@ -83,7 +86,7 @@ void AddPipes(vector<pipe>& pipes) {
 	pipe p;
 	int pipesId;
 		while (true) {
-			pipesId = random(1, 999);
+			pipesId = random(1, 99);
 			if (checkId(pipesId, pipes)) {
 				p.id = pipesId;
 				break;
@@ -91,6 +94,11 @@ void AddPipes(vector<pipe>& pipes) {
 
 		}
 		std::cout << "Pipe id: " << p.id << "\n";
+
+		cout << "Enter the name: ";
+		cin.ignore(32767, '\n');
+		getline(cin, p.name);
+
 		std::cout << "Enter the diametr: ";
 		p.diametr = rightValue();
 
@@ -127,6 +135,7 @@ struct station {
 	int workshops;
 	int WorkshopsInOperation;
 	int efficiency;
+	int percentWNIO;
 };
 
 bool checkId(int& Id, vector<station>& stations) {
@@ -171,7 +180,7 @@ void AddStations(vector<station>& stations) {
 	station s;
 	int stationsId;
 	while (true) {
-		stationsId = random(1, 999);
+		stationsId = random(1, 99);
 		if (checkId(stationsId, stations)) {
 			s.id = stationsId;
 			break;
@@ -208,11 +217,16 @@ void AddStations(vector<station>& stations) {
 	std::cout << "Enter the efficiency (%): ";
 	s.efficiency = rightValue();
 
+	std::cout << "Percent of not working workshops: ";
+	s.percentWNIO = ((s.workshops - s.WorkshopsInOperation) / s.workshops) * 100;
+	std::cout << s.percentWNIO << endl;
+
 	while (s.efficiency < 0 || s.efficiency > 100) {
 		std::cout << "The value must be >= 0 and <= 100. Try again: ";
 		s.efficiency = rightValue();
 		continue;
 	}
+
 	cout << "\nStation was added.\n";
 	stations.push_back(s);
 }
@@ -222,21 +236,23 @@ void Output(vector<pipe>& pipes, vector<station>& stations) {
 	cout << "\nPipes: \n";
 	for (int i = 1; i < pipes.size() + 1; ++i) {
 		if (pipes.size() != 0) {
-			cout << "\nId: " << pipes[i-i].id;
-			cout << "\nDiametr: " << pipes[i-i].diametr;
-			cout << "\nLength: " << pipes[i-i].length;
-			cout << "\nRepair ('yes' - 1, 'no' - 2): " << pipes[i-i].repair << "\n";
+			cout << "\nId: " << pipes[i - i].id;
+			cout << "\nName: " << pipes[i - i].name;
+			cout << "\nDiametr: " << pipes[i - i].diametr;
+			cout << "\nLength: " << pipes[i - i].length;
+			cout << "\nRepair ('yes' - 1, 'no' - 2): " << pipes[i - i].repair << "\n";
 			continue;
 		}
 	}
 	cout << "\nStations: \n";
 	for (int i = 1; i < stations.size() + 1; ++i) {
 		if (stations.size() != 0) {
-			cout << "\nId: " << stations[i-i].id;
-			cout << "\nName: " << stations[i-i].name;
-			cout << "\nWorkshops: " << stations[i-i].workshops;
-			cout << "\nWorkshops In Operation: " << stations[i-i].WorkshopsInOperation;
-			cout << "\nEfficiency: " << stations[i-i].efficiency << "\n";
+			cout << "\nId: " << stations[i - i].id;
+			cout << "\nName: " << stations[i - i].name;
+			cout << "\nWorkshops: " << stations[i - i].workshops;
+			cout << "\nWorkshops In Operation: " << stations[i - i].WorkshopsInOperation;
+			cout << "\nPercent of not working workshops: " << stations[i - i].percentWNIO;
+			cout << "\nEfficiency: " << stations[i - i].efficiency << "\n";
 			continue;
 		}
 	}
@@ -249,21 +265,23 @@ void Save(vector<pipe>& pipes, vector<station>& stations) {
 		for (int i = 1; i < pipes.size() + 1; i++) {
 		    if (pipes.size() != 0) {
 				file << "\nPipes: \n";
-				file << "\nId: " << pipes[i-i].id;
-				file << "\nDiametr: " << pipes[i-i].diametr;
-				file << "\nLength: " << pipes[i-i].length;
-				file << "\nRepair ('yes' - 1, 'no' - 2): " << pipes[i-i].repair << "\n";
+				file << "\nId: " << pipes[i - i].id;
+				file << "\nName: " << pipes[i - i].name;
+				file << "\nDiametr: " << pipes[i - i].diametr;
+				file << "\nLength: " << pipes[i - i].length;
+				file << "\nRepair ('yes' - 1, 'no' - 2): " << pipes[i - i].repair << "\n";
 			}
 		}
 
 		for (int i = 1; i < stations.size() + 1; i++) {
 		    if (stations.size() != 0) {
 				file << "\nStations: \n";
-				file << "\nId: " << stations[i-i].id;
-				file << "\nName: " << stations[i-i].name;
-				file << "\nWorkshops: " << stations[i-i].workshops;
-				file << "\nWorkshops In Operation: " << stations[i-i].WorkshopsInOperation;
-				file << "\nEfficiency: " << stations[i-i].efficiency << "\n";
+				file << "\nId: " << stations[i - i].id;
+				file << "\nName: " << stations[i - i].name;
+				file << "\nWorkshops: " << stations[i - i].workshops;
+				file << "\nWorkshops In Operation: " << stations[i - i].WorkshopsInOperation;
+				file << "\nPercent of not working workshops: " << stations[i - i].percentWNIO;
+				file << "\nEfficiency: " << stations[i - i].efficiency << "\n";
 			}
 		}
 		file.close();
@@ -284,6 +302,108 @@ int Load(vector<pipe>& pipes, vector<station>& stations) {
 		cout << strInput << endl;
 	}
 	return 0;
+}
+
+void SearchPipe(vector<pipe>& pipes) {
+	if (pipes.size() != 0) {
+		int choose;
+		string pipesName;
+		int pipesRepair;
+		cout << "Choose the filter:\n";
+		cout << "1 - Name, 2 - Repair: ";
+		choose = rightValue();
+		while (choose < 1 || choose > 2) {
+			cout << "Please enter 1(name) or 2(repair): ";
+			continue;
+		}
+
+		switch (choose) {
+		case 1: 
+			cout << "Enter the name of pipe: ";
+			cin.ignore(32767, '\n');
+			getline(cin, pipesName);
+			for (int i = 1; i < pipes.size() + 1; i++) {
+				if (pipes[i - i].name == pipesName) {
+					cin >> pipes[i - i].id;
+					cin >> pipes[i - i].name;
+					cin >> pipes[i - i].diametr;
+					cin >> pipes[i - i].length;
+					cin >> pipes[i - i].repair;
+				}
+			}
+		case 2: 
+			cout << "Is pipe under repair? (1 - yes, 2 - no): ";
+			pipesRepair = rightValue();
+			while (pipesRepair < 1 || pipesRepair > 2) {
+				cout << "Please enter 1 - yes or 2 - no: ";
+				continue;
+			}
+			for (int i = 1; i < pipes.size() + 1; i++) {
+				if (pipes[i - i].repair == pipesRepair) {
+					cin >> pipes[i - i].id;
+					cin >> pipes[i - i].name;
+					cin >> pipes[i - i].diametr;
+					cin >> pipes[i - i].length;
+					cin >> pipes[i - i].repair;
+				}
+			}
+		}
+	}
+	else {
+		cout << "Pipes wasn't added.";
+	}
+}
+
+void SearchStation(vector<station>& stations) {
+	if (stations.size() != 0) {
+		int choose;
+		string stationsName;
+		int percent;
+		cout << "Choose the filter:\n";
+		cout << "1 - Name, 2 - Percent of not working workshops: ";
+		choose = rightValue();
+		while (choose < 1 || choose > 2) {
+			cout << "Please enter 1(name) or 2(percent of not working percent): ";
+			continue;
+		}
+
+		switch (choose) {
+		case 1:
+			cout << "Enter the name of station: ";
+			cin.ignore(32767, '\n');
+			getline(cin, stationsName);
+			for (int i = 1; i < stations.size() + 1; i++) {
+				if (stations[i - i].name == stationsName) {
+					cin >> stations[i - i].id;
+					cin >> stations[i - i].name;
+					cin >> stations[i - i].workshops;
+					cin >> stations[i - i].WorkshopsInOperation;
+					cin >> stations[i - i].percentWNIO;
+					cin >> stations[i - i].efficiency;
+				}
+			}
+		case 2:
+			cout << "Enter the percent of not working workshops: ";
+			percent = rightValue();
+			while (percent < 0 || percent > 100) {
+				cout << "The value must be >=0 and <=100. Try again: ";
+				continue;
+			}
+			for (int i = 1; i < stations.size() + 1; i++) {
+				if (stations[i - i].percentWNIO == percent) {
+					cin >> stations[i - i].id;
+					cin >> stations[i - i].name;
+					cin >> stations[i - i].workshops;
+					cin >> stations[i - i].WorkshopsInOperation;
+					cin >> stations[i - i].percentWNIO;
+					cin >> stations[i - i].efficiency;
+				}
+			}
+		}
+	}
+	else {
+		cout << "Stations wasn't added.";
+	}
 }
 
 int main()
@@ -320,6 +440,10 @@ int main()
 		case 7:
 			Load(pipes, stations);
 			continue;
+		case 8:
+			SearchPipe(pipes);
+		case 9:
+			SearchStation(stations);
 		case 0:
 			While = false;
 			break;
